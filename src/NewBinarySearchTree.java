74cda7c526cf6ddec98a8a147083419c3bcde119
Jason <github@jasongetsemail.com>
2023-04-02 11:50:34 -0400

    Followed the guide for the remove() method. This is something where I understand the logic of what to do, but need to spend more time with the rules around which node replaces the parent of the node to be removed.

--- src/NewBinarySearchTree.java
+++ src/NewBinarySearchTree.java
@@ -49,7 +49,67 @@ public class NewBinarySearchTree {
     // if match found update pointer of previous to pointer of right node
 
     public void remove(int value) {
+        if (root == null) {
+            return;
+        }
+        Node traversalNode = this.root;
+        Node parentNode = null;
 
+        while (traversalNode != null) {
+            if (value < traversalNode.getValue()) {
+                parentNode = traversalNode;
+                traversalNode = traversalNode.getLeft();
+            } else if (value > traversalNode.getValue()) {
+                parentNode = traversalNode;
+                traversalNode = traversalNode.getRight();
+            } else if (value == traversalNode.getValue()) {
+                /*Option 1*/if (traversalNode.getRight() == null) {
+                    if (parentNode == null) {
+                        this.root = traversalNode.getLeft();
+                    } else {
+                        if (traversalNode.getValue() < parentNode.getValue()) {
+                            parentNode.setLeft(traversalNode.getLeft());
+                        } else if (traversalNode.getValue() > parentNode.getValue()) {
+                            parentNode.setRight(traversalNode.getLeft());
+                        }
+                    }
+                } /*Option 2*/else if (traversalNode.getRight().getLeft() == null) {
+                    if (parentNode == null) {
+                        this.root = traversalNode.getLeft();
+                    } else {
+                        traversalNode.getRight().setLeft(traversalNode.getLeft());
+                        if (traversalNode.getValue() < parentNode.getValue()) {
+                            parentNode.setLeft(traversalNode.getRight());
+                        } else if (traversalNode.getValue() > parentNode.getValue()) {
+                            parentNode.setRight(traversalNode.getRight());
+                        }
+                    }
+                } /*Option 3*/else {
+                    Node leftmostNode = traversalNode.getRight().getLeft();
+                    Node leftmostParentNode = traversalNode.getRight();
+                    while (leftmostNode.getLeft() != null) {
+                        leftmostParentNode = leftmostNode;
+                        leftmostNode = leftmostNode.getLeft();
+                    }
+
+                    leftmostParentNode.setLeft(leftmostNode.getRight());
+                    leftmostNode.setLeft(traversalNode.getLeft());
+                    leftmostNode.setRight(traversalNode.getRight());
+
+                    if (parentNode == null) {
+                        this.root = leftmostNode;
+                    } else {
+                        if (traversalNode.getValue() < parentNode.getValue()) {
+                            parentNode.setLeft(leftmostNode);
+                        } else if (traversalNode.getValue() > parentNode.getValue()) {
+                            parentNode.setRight(leftmostNode);
+                        }
+                    }
+                }
+                numberOfNodes--;
+                return;
+            }
+        }
     }
 
     private Node attemptAdd(Node traversalNode, Node toAdd) {
